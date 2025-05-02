c0bdd3b7105aac1ed8a2f6de54d93015814a0124
MeetTRIOS <MeetKumar.Valand@triosstudent.com>
2025-05-02 19:07:59 -0300

    new

---
+++ Mee.py
@@ -0,0 +1,20 @@
+#Iteration using For Loops
+a = ["Ironman", "Thor", "Captain America", "Hulk"]
+for i in a:
+    print(i)
+
+#Iteration using for Loops with Range and Length function
+for i in range(len(a)):
+    print(a[i])
+
+
+#Iteration using for while Loops
+i=0
+while i<(len(a)):
+    print(a[i])
+    i+=1
+
+#Using Short-Hand For Loop
+[print(i) for i in a]
+
+
